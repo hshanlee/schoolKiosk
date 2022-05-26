461c93f7c0e8cd238111cbeb22ff69971ac1bf91
hshanlee <96091403+hshanlee@users.noreply.github.com>
2022-05-27 04:16:02 +0900

    V0.1 - Clock and menu

---
+++ js/about-legacy.5e2a6c6d.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkkiosk"]=self["webpackChunkkiosk"]||[]).push([[443],{9030:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],a=n(1001),l={},r=(0,a.Z)(l,s,u,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about-legacy.5e2a6c6d.js.map
